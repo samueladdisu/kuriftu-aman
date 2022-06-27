f1b58f18cda54abc5b255d8e5efbcb267a8f013e
samuel addisu <samueladdisu7@gmail.com>
2022-06-27 13:40:01 +0300

    video fix

--- components/Hero.vue
+++ components/Hero.vue
@@ -7,19 +7,12 @@
       </div> -->
       <img v-if="src" :src="require('@/assets/img/' + src + '')" alt="" />
       <div v-if="video" class="video-wrapper">
-        <div style="padding: 56.25% 0 0 0; position: relative">
+        <div class="vimeo-container">
           <iframe
             :src="
               'https://player.vimeo.com/video/' +
               video +
               '?api=1&autoplay=1&controls=0&sidedock=0&loop=1&title=0&byline=0&portrait=0&muted=1&background=1'
-            "
-            style="
-              position: absolute;
-              top: 0;
-              left: 0;
-              width: 100%;
-              height: 100%;
             "
             frameborder="0"
             allow="autoplay; fullscreen; picture-in-picture"
@@ -41,12 +34,21 @@ export default {
 <style lang="scss">
 .display-video {
   padding-top: 4rem;
-  // .container {
-  //   .video-wrapper {
-  //     display: grid;
-  //     place-items: center;
-  //   }
-  // }
+  .container {
+    .video-wrapper {
+      .vimeo-container {
+        padding: 56.25% 0 0 0;
+        position: relative;
+        iframe {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+  }
 }
 @include responsive($md) {
   .display-video {
